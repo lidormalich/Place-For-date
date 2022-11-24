6b270aca0b58451f95bc60825855d81c14255f71
Lidor Repo <lidormalich@gmail.com>
2022-11-24 12:06:52 +0200

    update

--- js/search.js
+++ js/search.js
@@ -62,22 +62,37 @@ function showStudentsCard() {
             let rowLenght = data.table.rows.length;
             let colLenght = data.table.cols.length;
             for (let i = 0; i < rowLenght; i++) {
-                document.getElementById("cardDive").innerHTML += `<div class="card col-sm-12 col-md-4 col-lg-3 mb-2 mx-auto" style="width: 18rem;">    
-                
-
-
-                                <div class="card-body">
-                                    <h5 class="card-title">${sliceData(JSON.stringify(data.table.rows[i].c[4]))}</h5>
+                let someKnow = sliceData(JSON.stringify(data.table.rows[i].c[2]));
+                document.getElementById("cardDive").innerHTML += `<div class="card col-sm-12 col-md-4 col-lg-3 mb-2 mx-auto" style="width: 18rem;"> 
+                                <div class="card-body"><h5 class="card-title">${sliceData(JSON.stringify(data.table.rows[i].c[4]))}</h5>
+                                <div class="container">
+                                <div class="row">
+                                <div class="col-sm">
+                                <h6 class="card-text leftSide">אזור: ${sliceData(JSON.stringify(data.table.rows[i].c[0]))}</h6>
+                                </div>
+                                <div class="col-sm">
+                                <p class="h6 cityFont "><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                                    <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
+                                    <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                                    </svg>
+                                    ${sliceData(JSON.stringify(data.table.rows[i].c[5]))}</p>
+                                </div>
+                                </div>
+                                </div>
+                                    
+                                    
+                                    
                                     <img src="img/gmap.png"  class="card-img-top" alt="...">
-                                    <a href='${sliceData(JSON.stringify(data.table.rows[i].c[1]))}' class="card-text text-center">נווט בעזרת Google Map</a> 
-                                    <p class="card-text">2: ${sliceData(JSON.stringify(data.table.rows[i].c[2]))}</p> 
-                                    <p class="card-text">3: ${sliceData(JSON.stringify(data.table.rows[i].c[3]))}</p> 
-                                    <p class="card-text">4: ${sliceData(JSON.stringify(data.table.rows[i].c[4]))}</p> 
-                                    <p class="card-text">5: ${sliceData(JSON.stringify(data.table.rows[i].c[5]))}</p> 
-                                    <p class="card-text">0: ${sliceData(JSON.stringify(data.table.rows[i].c[0]))}</p> 
+                                    <a href='${sliceData(JSON.stringify(data.table.rows[i].c[1]))}' class="card-text justify-content center align-items center redLink">♡ נווט בעזרת Google Map ♡</a> 
+                                    <p class="card-text">כמה מילים על המקום: ${someKnow.substring(0, 200)}</p> 
+
+
+                                    <p class="card-text">תגיות: ${sliceData(JSON.stringify(data.table.rows[i].c[3]))}</p> 
                                     <a href="" class="btn btn-primary" onclick="showProduct(${i})">נשמע דייט מעולה, תראו פרטים</a>
                                 </div>
                                 </div> `;
+
+                // <p class="card-text">עיר: ${sliceData(JSON.stringify(data.table.rows[i].c[5]))}</p>
                 let city2 = "";
                 city2 = sliceData(JSON.stringify(data.table.rows[i].c[5]));
 
